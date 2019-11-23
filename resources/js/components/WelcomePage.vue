7880c0d62aa94ba2ce6f9ddbc9e0ec54e2641ff3
hiromitsu sasaki <hiromitsu.sasaki@gmail.com>
2019-11-23 11:54:18 +0900

    add WelcomePage component

---
+++ resources/js/components/WelcomePage.vue
@@ -0,0 +1,29 @@
+<template>
+    <section class="section">
+        <div class="container">
+            <h1 class="h1">Welcome</h1>
+        </div>
+        <div class="container">
+            <b-field label="please put your name">
+                <b-input v-model="name"></b-input>
+            </b-field>
+            <b-button @click="clicked">Click me</b-button>
+        </div>
+    </section>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            name: "",
+        }
+    },
+    methods: {
+        clicked() {
+            this.$buefy.notification.open({
+                message: 'Hi ' + this.name + '. Nice to meet you!!',
+                })
+        }
+    }
+}
+</script>
